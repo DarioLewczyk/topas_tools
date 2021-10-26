24a2a794e3c4c8dc89f9d04efe576cb82fafde8c
DarioLewczyk <christopherlewczyk@Darios-15-in-MacBook-Pro.local>
2021-10-26 13:53:03 -0400

    I have successfully fixed the python_to_topas_rietveld module. Now everything works alright and the loading of the data also works for us.

--- python_to_topas_rietveld.py
+++ python_to_topas_rietveld.py
@@ -111,6 +111,7 @@ class Analyzer:
         self.data_dict = {} #creating a dictionary to hold our data.
         self.dict_of_data_files = {}
         self.csv_files_loaded = False ###########################Default value is false. Will change if csv files are detected. 
+        self.rietveld = False ################################## Default value is false  will change if csv files are loaded
         os.chdir(data_folder) #Changes us to the working directory
         for i, f in enumerate(os.listdir()):
             if f.endswith('.xy'):
@@ -144,6 +145,7 @@ class Analyzer:
             ####################
             if f.endswith('.csv'):
                 self.csv_files_loaded = True ###############################################ENSURES WE KNOW THAT CSV FILE DATA ARE PRESENT FOR PLOTTING
+                self.rietveld = True #######################################################
                 cols = ['Rwp', 'a', 'b', 'c', 'alpha', 'beta', 'gamma', 'volume']
                 csv_df = pd.read_csv(f, header = None, sep = '\t', names=cols)
                 #The format of the filename is: BiVO4_Fixed_Supercell_#_Results.csv. This just grabs the number. 
@@ -170,11 +172,9 @@ class Analyzer:
                     'volume':vol
                     })
     # Make Plots{{{
-    def make_plots(self,save_figs = False, plot_diff = False, rietveld = False):
-        with tqdm(total= len(self.data_dict)) as pbar:
-            print('Working on Figures...')
-            with tqdm(total = len(self.data_dict)) as pbar2:
-                print('Working on Saving Figures...')
+    def make_plots(self,save_figs = False, plot_diff = False):
+        with tqdm(total= len(self.data_dict), desc='Making Figures...') as pbar: 
+            with tqdm(total = len(self.data_dict), desc= 'Saving Figures...') as pbar2: 
             
                 for i, dict_entry in enumerate(self.data_dict.values()):
                     #This is where the xy file plots are made{{{
@@ -211,7 +211,7 @@ class Analyzer:
                     #Creating the plot titles. 
                     fn_list = fn_no_xy.split('_')
                     fn_list[0] = r'BiVO$_4$'
-                    if rietveld == True:
+                    if self.rietveld == True:
                         fn_list[-2] = 'Rietveld' #This replaces the word 'simulation' with 'Rietveld'  
                     title = ' '.join(fn_list)
                     if plot_diff == True:
@@ -228,17 +228,17 @@ class Analyzer:
                             os.mkdir(figure_directory)
                         os.chdir(figure_directory)
                         #####################################################
-                        if rietveld == True:
+                        if self.rietveld == True:
                             ###### Changes the name to rietveld. 
                             fig.savefig('BiVO4_Rietveld_{}.png'.format(dict_entry['number']))
-                        elif rietveld ==False:
+                        elif self.rietveld ==False:
                             fig.savefig('BiVO4_Pattern_Sim_{}.png'.format(dict_entry['number']))
                         #####################################################
                         if plot_diff == True:
-                            if rietveld == True:
+                            if self.rietveld == True:
                                 ##### Changes the name to rieteveld
                                 fig2.savefig('BiVO4_Rietveld_diff_{}.png'.format(dict_entry['number']))
-                            elif rietveld == False:
+                            elif self.rietveld == False:
                                 fig2.savefig('BiVO4_Pattern_Sim_diff_{}.png'.format(dict_entry['number']))
                         #####################################################
                         pbar2.update(1)
@@ -251,8 +251,8 @@ class Analyzer:
                 # Rwp Plot
                 ################
                 self.rwp_fig, self.rwp_ax = plt.subplots()
-            
-                self.rwp_ax.scatter(i,self.data_dict[i]['rwp'], color='b')
+                for i, v in enumerate(tqdm(self.data_dict, desc='Rwp Figure')):
+                    self.rwp_ax.scatter(i,self.data_dict[i]['rwp'], color='b')
                 self.rwp_ax.set_xlabel('Enumeration Figure')
                 self.rwp_ax.set_ylabel(r'R$_{wp}$')
                 self.rwp_ax.set_title(r'R$_{wp}$ Results')
@@ -260,8 +260,8 @@ class Analyzer:
                 # Volume Plot
                 #################
                 self.vol_fig, self.vol_ax = plt.subplots()
-               
-                self.vol_ax.scatter(i,self.data_dict[i]['volume'], color='b')
+                for i,v in enumerate(tqdm(self.data_dict,desc='Vol Figure')):
+                    self.vol_ax.scatter(i,self.data_dict[i]['volume'], color='b')
                 self.vol_ax.set_xlabel('Enumeration Figure')
                 self.vol_ax.set_ylabel(r'Volume $\AA^3$') # \AA adds the angstrom symbol. 
                 self.vol_ax.set_title('Volume Results')
@@ -298,29 +298,31 @@ class Analyzer:
             num_figs = 1
             figure_range = [1]
         self.figure_dictionary = {} #This creates a dictionary where the subplots will be kept. 
-        for i in figure_range:
-            subplot_fig = plt.figure(i) #Creates the figure object.
-            subplot_fig.clear() #Make sure that the plot is cleared before being saved. 
-            subplot_fig.set_size_inches(12,12.5) #Makes the figure a very large size (10 in. by 10 in.)
-            #plt.tight_layout(w_pad = 2,h_pad = 3)
-            '''
-            I think that I need to add all of the axes here rather than down below. 
-            This may preserve them a little bit better. I am not sure. May be worth trying. 
-            '''
-            self.figure_dictionary[i] = subplot_fig #we are adding each numbered figure to a dictionary of figures.
         '''
-        Here is where the subplots are made. Using a progress bar. 
+        Here is where all the plotting happens with a 
+        progress bar. 
         '''
-        with tqdm(total=len(figure_range)) as pbar:
-            print('Generating {} Plots'.format(num_figs))
+        with tqdm(total=len(figure_range),desc='Making Figures') as pbar:
+            #print('Generating {} Plots'.format(num_figs))
             for fig_num in figure_range:
-                #print('Working on Figure #{}'.format(fig_num))##################################
-                subplot_fig = self.figure_dictionary[fig_num] #we are calling the figure to add subplots to here.   
-                x_axis = r'$2{\theta}^\circ$'
-                y_axis = 'Intensity'
-                subplot_fig.text(0.5,0.04, x_axis, ha = 'center')
-                subplot_fig.text(0.04,0.5, y_axis, va = 'center', rotation='vertical')
+                '''
+                I am going to be adding the figures to the figure
+                dictionary right now. 
+                '''
+                subplot_fig = plt.figure(fig_num) #This makes a figure with the number of "fig_num"
+                subplot_fig.clear() #This clears the figure out. 
+                subplot_fig.set_size_inches(12,12.5) #Resize the overall large figure
              
+                self.figure_dictionary[fig_num] = {'fig_{}'.format(fig_num): subplot_fig} #This adds the figure to the figure dictionary
+                '''
+                Here is where we will add the axes. 
+                '''
+                x_axis = r'$2{\theta}^\circ$' #This is the label for x
+                y_axis = 'Intensity' # Label for y
+                self.figure_dictionary[fig_num]['fig_{}'.format(fig_num)].text(0.5,0.04, x_axis, ha = 'center')
+                self.figure_dictionary[fig_num]['fig_{}'.format(fig_num)].text(0.04,0.5, y_axis, va = 'center', rotation='vertical')
+                ############################################################################3
+                             
                 for i, f in enumerate(self.data_dict):
                     v =self.data_dict[i] #This ensures we go through the files in order. 
                     subplot_angle = v['angle']
@@ -328,28 +330,62 @@ class Analyzer:
                     subplot_y_calc = v['y_calc']
                     subplot_y_diff = v['y_diff']
                     subplot_modified_y_diff = v['modified_diff_curve']
-                    if fig_num!=1:
+                    ####################################
+                    # Corrected i and the if statements
+                    ####################################
+                    corrected_i = 0 #This is here to always reset the corrected_i.
+                    if fig_num == 1:
+                        if i+1 <= data_range_dictionary[fig_num]:
+                            corrected_i = i+1 #This sets the value to a more sensible one
+                            #print('corrected_i (1): {}'.format(corrected_i))
+                            #print('regular i: {}'.format(i)) #################################################3
+                            subplot_ax = self.figure_dictionary[fig_num]['fig_{}'.format(fig_num)].add_subplot(rows,cols,corrected_i) 
+                            ####################################3
+                            # Now we are going to add the axis to the dict. 
+                            #####################################
+                            self.figure_dictionary[fig_num].update({
+                                'ax_{}'.format(corrected_i): subplot_ax
+                                })
+                            ####################################
+
+                    elif fig_num!=1:
                         if i+1<=data_range_dictionary[fig_num] and i+1>data_range_dictionary[fig_num-1]:
+                            corrected_i = i - data_range_dictionary[fig_num-1] +1
+                            #print('corrected_i (!1): {}'.format(corrected_i))
                             #print('regular i: {}'.format(i))
                             #We have to do this because i becomes > the number of boxes we have in an nxm plot
-                            corrected_i = i - data_range_dictionary[fig_num-1]
+                            
                             #print('corrected i:{}'.format(corrected_i)) #######################################
-                            subplot_ax = subplot_fig.add_subplot(rows,cols,corrected_i+1) 
-                            print('Row: {}, Col: {}, index: {}'.format(rows,cols,corrected_i+1))
-                    elif fig_num == 1:
-                        if i+1 <= data_range_dictionary[fig_num]:
-                            #print('regular i: {}'.format(i)) #################################################3
-                            subplot_ax = subplot_fig.add_subplot(rows,cols,i+1) 
-                    if show_diff == True:
-                        subplot_ax.plot(subplot_angle, subplot_modified_y_diff, 'grey')#This plots the difference curve. 
-                    subplot_ax.plot(subplot_angle,subplot_y_obs,'b') #Plots the observed pattern
-                    subplot_ax.plot(subplot_angle,subplot_y_calc,'r') #Plots the calculated pattern
+                            subplot_ax = self.figure_dictionary[fig_num]['fig_{}'.format(fig_num)].add_subplot(rows,cols,corrected_i) 
+                            #print('Row: {}, Col: {}, index: {}'.format(rows,cols,corrected_i+1))
+                            ########################################
+                            # Now we are adding this axis to the dictionary
+                            ########################################
+                            self.figure_dictionary[fig_num].update({
+                                'ax_{}'.format(corrected_i): subplot_ax
+                                })
+                    ######################################
+                    # Subplots are plotted
+                    # The if statement for the corrected_i variable must be here. 
+                    ######################################
+                    if corrected_i:
+                        #This means that if corrected_i!=0 then do these things. 
 
-                    plt.tight_layout(w_pad=3,h_pad=3)#This adds enough space between the plots so that nothing gets cut off. 
-                    subplot_ax.ticklabel_format(axis='y', style='sci',scilimits=(0,0)) #This is here to force scientific notation for the y axis.
-                    title = r"BiVO$_4$ {}".format(v['number'])
-                 
-                    subplot_ax.set_title(title)
+                        if show_diff == True:
+                            self.figure_dictionary[fig_num]['ax_{}'.format(corrected_i)].plot(subplot_angle, subplot_modified_y_diff, 'grey')#This plots the difference curve. 
+                        self.figure_dictionary[fig_num]['ax_{}'.format(corrected_i)].plot(subplot_angle,subplot_y_obs,'b') #Plots the observed pattern
+                        self.figure_dictionary[fig_num]['ax_{}'.format(corrected_i)].plot(subplot_angle,subplot_y_calc,'r') #Plots the calculated pattern
+                        ######################################
+            
+                        plt.tight_layout(w_pad=3,h_pad=3)#This adds enough space between the plots so that nothing gets cut off. 
+                        #####################################
+                        # Subplot Decorations
+                        #####################################
+                        self.figure_dictionary[fig_num]['ax_{}'.format(corrected_i)].ticklabel_format(axis='y', style='sci',scilimits=(0,0)) #This is here to force scientific notation for the y axis.
+                        title = r"BiVO$_4$ {}".format(v['number'])   
+                        self.figure_dictionary[fig_num]['ax_{}'.format(corrected_i)].set_title(title)
+
+                self.figure_dictionary[fig_num]['fig_{}'.format(fig_num)]; #I think this will refresh the figure. Not sure its necessary
                 pbar.update(1)
         
         if save_figs == True:
@@ -359,10 +395,9 @@ class Analyzer:
             else:
                 os.mkdir(figure_directory)
             os.chdir(figure_directory)
-            with tqdm(total = len(figure_range)) as pbar2:
-                print('Saving Figures...')
-                for i, subplot_fig in enumerate(self.figure_dictionary.values()):
-                    subplot_fig.savefig('BVO_{}_x_{}_grid_{}.png'.format(rows,cols,i))
+            with tqdm(total = len(figure_range),desc='Saving Figures') as pbar2: 
+                for i, fig_number in enumerate(self.figure_dictionary):
+                    self.figure_dictionary[fig_number]['fig_{}'.format(fig_number)].savefig('BVO_{}_x_{}_grid_{}.png'.format(rows,cols,i))
                     pbar2.update(1)
                 os.chdir(self.data_folder)
              
@@ -390,6 +425,7 @@ class Analyzer:
         pickle.dump(class_object, file_object) #saves everything else.
         
         file_object.close()
+        os.chdir(self.data_folder) #This will return us to the working dir. 
     #}}}
 #}}}
 
